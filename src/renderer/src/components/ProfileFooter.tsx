2c2f72c68a4692c6b8d00292fc9db82c65fd26e3
Sadiq Salau <sadiqsalau888@gmail.com>
2026-01-08 14:54:58 +0100

    feat: Centered the action list

--- src/renderer/src/components/ProfileFooter.tsx
+++ src/renderer/src/components/ProfileFooter.tsx
@@ -1,27 +1,26 @@
+import {} from 'solid-icons/hi'
+
+import {} from './WebviewButton'
+
 import { Component, Show } from 'solid-js'
-import { HiSolidBars3 } from 'solid-icons/hi'
 import { useBrowserProfileContext } from '@renderer/hooks/useBrowserProfileContext'
 
 import BrowserActionList from './BrowserActionList'
-import { WebviewButton } from './WebviewButton'
 
 const ProfileFooter: Component = () => {
   const context = useBrowserProfileContext()
 
   return (
-    <div class="shrink-0 flex flex-wrap items-center justify-between p-1 gap-2">
-      <div class="border border-slate-200 dark:border-slate-600 rounded-full p-1 px-2 overflow-auto">
-        <Show when={context.selectedTab()}>
+    <Show when={context.selectedTab()}>
+      <div class="shrink-0 flex flex-wrap items-center justify-center p-1 gap-2">
+        <div class="border border-slate-200 dark:border-slate-600 rounded-full p-1 px-2 overflow-auto">
           <BrowserActionList
             partition={`persist:profile-${context.profile().id}`}
             tab={context.selectedTab()?.webContentsId}
           />
-        </Show>
+        </div>
       </div>
-      <WebviewButton>
-        <HiSolidBars3 />
-      </WebviewButton>
-    </div>
+    </Show>
   )
 }
 
